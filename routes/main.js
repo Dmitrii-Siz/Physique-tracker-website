bf9264c6f6af5519250219db6390836e758bdcdf
DmitriiUser <107858231+DmitriiUser@users.noreply.github.com>
2023-12-06 00:21:22 +0000

    Register and Login page Errors Added
    Added some error handling for the register page and the login page

--- routes/main.js
+++ routes/main.js
@@ -23,27 +23,22 @@ module.exports = function(app, shopData) {
     
 
     //logout:
-    app.get('/logout', redirectLogin, (req,res) => {
-        req.session.destroy(err => {
-        if (err) {
-          return res.redirect('./')
-        }
-        res.send('you are now logged out. <a href='+'./'+'>Home</a>');
-        })
+     app.get('/logout', redirectLogin, (req,res) => {
+         req.session.destroy(err => {
+         if (err) {
+           return res.redirect('./')
+         }
+            res.redirect('/');
+         })
     })
-
 
     // Handle our routes
     app.get('/',function(req,res){
         res.render('index.ejs', shopData)
     });
 
-    app.get('/profile',redirectLogin,function(req,res){
-        res.render("profile.ejs", shopData);
-    });
-
     app.get('/register', function (req,res) {
-        res.render('register.ejs', shopData);                                                                     
+        res.render('register.ejs', { shopData, errorMessage: null });                                                                    
     });                                                                                                 
     app.post('/registered',[
         check('email').isEmail().normalizeEmail(),
@@ -52,34 +47,51 @@ module.exports = function(app, shopData) {
         //enter correct email or redirect back to register page
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            res.redirect('./register'); }
+            return res.render('register.ejs', { shopData, errorMessage: 'Something Went Wrong, please check that you have filled in all the form components' }); }
         else {
         const bcrypt = require('bcrypt');
         const saltRounds = 10;
         const plainPassword = req.body.password;
-        
+        if(plainPassword.length < 8){
+            return res.render('register.ejs', { shopData, errorMessage: 'Password must be at least 8 characters in length' });
+        }
         const first = req.sanitize(req.body.first)//validates the first name
         const last = req.sanitize(req.body.last)//validates the last name
         const email = req.sanitize(req.body.email)//sanitises email
         const username = req.sanitize(req.body.username)//sanitises username
 
-    bcrypt.hash(plainPassword, saltRounds).then(hashedPassword => {
-        // Construct the SQL query after password hashing is complete
-        let sqlquery = `INSERT INTO users (username, first_name, last_name, email, hashedPassword) VALUES (?, ?, ?, ?, ?)`;
+        // Check if the username already exists in the database
+        let sqlquery = `SELECT * FROM users WHERE username = ?`;
 
-        // Execute the query and handle the result
-        db.query(sqlquery, [username, first, last, email, hashedPassword], (err, result) => {
+        // username already exists?
+        db.query(sqlquery, [username], (err, result) => {
             if (err) {
-                res.redirect('./');
-            } else {
-            // Data inserted successfully:
-            result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email;
-            result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
-            res.send(result);
+                // Handle the database error, for example, redirect to the registration page with an error message.
+                return res.render('register.ejs', { shopData, errorMessage: 'There seems to be an error with our system, please try again later' });
+            }
+            if (result.length > 0) {
+                // Username already exists
+                return res.render('register.ejs', { shopData, errorMessage: 'Sorry, username already exists' });
             }
         });
-        })
-    }
+
+        bcrypt.hash(plainPassword, saltRounds).then(hashedPassword => {
+            // Construct the SQL query after password hashing is complete
+            let sqlquery = `INSERT INTO users (username, first_name, last_name, email, hashedPassword) VALUES (?, ?, ?, ?, ?)`;
+
+            // Execute the query and handle the result
+            db.query(sqlquery, [username, first, last, email, hashedPassword], (err, result) => {
+                if (err) {
+                    return res.render('register.ejs', { shopData, errorMessage: 'There seems to be an error with our system, please try again later' });
+                } else {
+                // Data inserted successfully:
+                result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email;
+                result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
+                return res.send(result);
+                }
+            });
+            })
+        }
     });
 
     //track your physique page:
@@ -92,11 +104,22 @@ module.exports = function(app, shopData) {
         res.render('manage_calories.ejs', shopData)
     });
 
+    //view progress page:
+    app.get('/loggedin',redirectLogin, function(req,res){
+        res.render('loggedin.ejs', shopData)
+    });
+
 
     //login page backend:
     app.get('/login', function (req,res) {
-        res.render('login.ejs', shopData);                                                                     
-    }); 
+        res.render('login.ejs', { shopData, errorMessage: null });                                                                  
+    });
+
+
+    //logged in:
+    app.get('loggedin',redirectLogin,function(req,res){
+        next ();
+    });
 
     app.post('/loggedin', function (req,res) {
         //bcrypt stuff
@@ -111,7 +134,8 @@ module.exports = function(app, shopData) {
         //get the username for the database and compare the passwords:
         db.query(sqlquery, (err,response) =>{
             if(err){
-                res.send('Username was not found!')
+                //error message displayed if the login/password is incorrect
+                res.render("login.ejs", { shopData, errorMessage: "Incorrect username or password" });
             }else{
                 //username found successfully:
                 hashedPassword = response[0].hashedPassword;
@@ -132,7 +156,8 @@ module.exports = function(app, shopData) {
                         res.render("loggedin.ejs", { username: username });
                     }
                     else {
-                        res.send("Error, incorrect username or password")
+                        //error message displayed if the login/password is incorrect
+                        res.render("login.ejs", { shopData, errorMessage: "Incorrect username or password" });
                     }
                 });
             }
